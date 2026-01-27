36330ae32a451584e56ed25984b7ca8c3455d44f
Acerx-AMJ <ajm.acerx@gmail.com>
2026-01-27 17:19:59 +0200

    Mobile fixes and style changes.

--- source/projects.js
+++ source/projects.js
@@ -18,38 +18,30 @@ document.addEventListener("DOMContentLoaded", () => {
     for (const project of projects) {
         // Create the divs
         let div = document.createElement("div")
-        div.className = "m-5 py-4 px-2 bg-zinc-800 rounded-xl flex flex-row items-center justify-center"
-
-        let leftdiv = document.createElement("div")
-        leftdiv.className = "w-1/3 h-full bg-zinc-700 rounded-xl mx-2"
-        div.appendChild(leftdiv)
-
-        let rightdiv = document.createElement("div")
-        rightdiv.className = "w-2/3 h-full bg-zinc-700 rounded-xl mx-2 flex items-center justify-center"
-        div.appendChild(rightdiv)
+        div.className = "m-5 py-4 px-2 bg-zinc-800 rounded-xl transition transition-duration-100 hover:bg-gray-800"
 
         // Create the elements
         let header = document.createElement("h1")
         header.href = project.link
         header.innerText = project.title
-        header.className = "m-5 text-heading text-3xl font-bold text-zinc-50"
-        leftdiv.appendChild(header)
-
-        let description = document.createElement("p")
-        description.innerHTML = `<span style="margin-left:20px"></span>${project.description}`
-        description.className = "mx-3 text-body text-zinc-50"
-        leftdiv.appendChild(description)
+        header.className = "mt-3 ml-3 text-heading text-3xl font-bold text-zinc-50"
+        div.appendChild(header)
 
         let image = document.createElement("img")
         image.src = project.image
         image.className = "p-5 rounded-4xl"
-        rightdiv.appendChild(image)
+        div.appendChild(image)
+
+        let description = document.createElement("p")
+        description.innerHTML = `<span style="margin-left:20px"></span>${project.description}`
+        description.className = "mx-3 text-body text-zinc-50"
+        div.appendChild(description)
 
         let link = document.createElement("a")
         link.href = project.link
         link.innerText = "Learn More"
         link.className = "mx-3 my-3 font-medium text-sky-400 hover:underline"
-        leftdiv.appendChild(link)
+        div.appendChild(link)
 
         projectdiv.appendChild(div)
     }
